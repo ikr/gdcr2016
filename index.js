1459381ffd3db9ced2a16dc9378ae8ff544438ad
Ivan Krechetov <ikr@ikr.su>
2016-10-22 11:28:40 +0200

    Paper test plan session
    First top-down, then bottom-up along the wayg

--- index.js
+++ index.js
@@ -9,26 +9,84 @@ const assert = require('assert'),
 // 4. Dead cell with exactly 3 live neighbours becomes a live cell
 //------------------------------------------------------------------------------
 
-const game = {};
+function newGeneration(currentGeneration) {
+    return currentGeneration;
+}
+
+function livingNeighboursCount() {
+    return 0;
+}
+
+function allAdjacentCoordinates() {
+    const result = [];
+
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if (i === 0 && j === 0) continue;
+            result.push([i, j]);
+        }
+    }
+
+    return result;
+}
 
 //------------------------------------------------------------------------------
 
-describe('something abstract', () => {
-    it('is abstract', () => {
-        assert(true);
+
+describe('newGeneration', () => {
+    it('is identical on an empty generation', () => {
+        assert.deepEqual(newGeneration([]), []);
     });
 
-    it('is concrete beyond decency', () => {
-        assert.strictEqual(2, 2);
+    it('is identical on a 3-cell triangle', () => {
+        assert.deepEqual(
+            newGeneration([[0, 0], [0, 1], [1, 1]]),
+            [[0, 0], [0, 1], [1, 1]]
+        );
     });
 
-    it('has nothing to do with this calculation', () => {
-        assert.deepEqual(R.map(R.inc)([2]), [3]);
+    it.skip('is empty on a lonely cell', () => {
+        assert.deepEqual(newGeneration([[0, 0]]), []);
     });
 });
 
-describe('the game', () => {
-    it('is an empty object', () => {
-        assert.deepEqual(game, {});
+describe('livingNeighboursCount', () => {
+    it('is 0 for an empty generation', () => {
+        assert.strictEqual(livingNeighboursCount([0, 0], []), 0);
+    });
+
+    it.skip('is 1 for a dead sibling of a living cell', () => {
+        assert.strictEqual(
+            livingNeighboursCount([0, 0], [[0, 1]]),
+            1
+        );
+    });
+});
+
+describe('allAdjacentCoordinates', () => {
+    it('is an enumeration of all the deltas for (0, 0)', () => {
+        assert.deepEqual(allAdjacentCoordinates([0, 0]), [
+            [ -1, -1 ],
+            [ -1, 0 ],
+            [ -1, 1 ],
+            [ 0, -1 ],
+            [ 0, 1 ],
+            [ 1, -1 ],
+            [ 1, 0 ],
+            [ 1, 1 ]
+        ]);
+    });
+
+    it('is correct for (1, 0)', () => {
+        assert.deepEqual(allAdjacentCoordinates([1, 0]), [
+            [ 0, -1 ],
+            [ 0, 0 ],
+            [ 0, 1 ],
+            [ 1, -1 ],
+            [ 1, 1 ],
+            [ 2, -1 ],
+            [ 2, 0 ],
+            [ 2, 1 ]
+        ]);
     });
 });
